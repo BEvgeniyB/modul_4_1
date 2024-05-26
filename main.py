acfcd57d35ba1b97f2b22c0914d330b2e7caee58
BEvgeniyB <masterbe2012@yandex.ru>
2024-05-26 20:24:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import function as ff
+from fake_math import divide as fake_d
+from true_math import divide as true_d
 
 ff.greeting_1()
 ff.greeting_2()
 print(__name__)
+
+result1 = fake_d(69, 3)
+result2 = fake_d(3, 0)
+result3 = true_d(49, 7)
+result4 = true_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
